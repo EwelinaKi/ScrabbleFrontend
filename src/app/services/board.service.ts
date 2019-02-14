31231aa1493f569c41b80b4105dd5addbd448b0a
Ewelina Kijanowska <kijanowska@gmail.com>
2019-02-14 15:33:10 +0100

    polish letters display BUGFIX

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Slot} from '../game/slot';
 import {Letter} from '../game/letter';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +11,39 @@ export class BoardService {
   imgPath = 'assets/img/letters';
   board = [];
   rack = [];
+  letterImages = {
+      a: 'A.png',
+      ą: 'AA.png',
+      b: 'B.png',
+      c: 'C.png',
+      ć: 'CC.png',
+      d: 'D.png',
+      e: 'E.png',
+      ę: 'EE.png',
+      f: 'F.png',
+      g: 'G.png',
+      h: 'H.png',
+      i: 'I.png',
+      j: 'J.png',
+      k: 'K.png',
+      l: 'L.png',
+      ł: 'LL.png',
+      m: 'M.png',
+      n: 'N.png',
+      ń: 'NN.png',
+      o: 'O.png',
+      ó: 'OO.png',
+      p: 'P.png',
+      r: 'R.png',
+      s: 'S.png',
+      ś: 'SS.png',
+      t: 'T.png',
+      u: 'U.png',
+      w: 'W.png',
+      z: 'Z.png',
+      ż: 'ZZ.png',
+      ź: 'ZZZ.png',
+}
 
   constructor() {
   }
@@ -39,7 +73,7 @@ export class BoardService {
   displayLettersInRack(letters: any) {
     letters.forEach(letter => {
       const slot = this.rack.find((obj) => !obj.letter.character);
-      slot.letter = new Letter(letter, `${this.imgPath}/${letter.toUpperCase()}.png`);
+      slot.letter = new Letter(letter, `${this.imgPath}/${this.letterImages[letter]}`);
     });
   }
 
