d47a0358c11d9855d3369f05696a0edcd16d0678
Ewelina Kijanowska <kijanowska@gmail.com>
2019-02-14 14:44:17 +0100

    exchange letters message

--- src/app/game/control/control.component.ts
+++ src/app/game/control/control.component.ts
@@ -3,6 +3,14 @@ import {BoardService} from '../../services/board.service';
 import {GameService} from '../../services/game.service';
 import {Letter} from '../letter';
 
+
+enum MessageTypes {
+  error = 'text-danger',
+  warning = 'text-warning',
+  success = 'text-success',
+  default = 'text-light'
+}
+
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
@@ -14,6 +22,7 @@ export class ControlComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.addNewMessage("Nowa gra rozpoczęta.", MessageTypes.warning );
   }
 
   checkMove() {
@@ -36,16 +45,27 @@ export class ControlComponent implements OnInit {
 
     this.gameService.exchangeLetters(lettersToExchange)
       .subscribe(
-      res => {
-        markedElementsInd.map(ind => this.boardService.getRack()[ind].letter = new Letter);
-        this.boardService.displayLettersInRack(res);
-      }, err => {
-        console.log('Could not exchange letters. Try again.');
-      }
-    );
+        res => {
+          markedElementsInd.map(ind => this.boardService.getRack()[ind].letter = new Letter);
+          this.boardService.displayLettersInRack(res);
+          this.addNewMessage(`Nowe litery: ${res}`, MessageTypes.default);
+        }, err => {
+          this.addNewMessage("Błąd połączenia. Spróbuj ponownie.", MessageTypes.error);
+        }
+      );
   }
 
   passMove() {
+
+  }
+
+  addNewMessage(message: string, messageType: MessageTypes) {
+    const p = document.createElement('p');
+    p.className = `${messageType}`;
+    p.innerHTML = `${message}`;
+    const messageBox = document.getElementById('messageBox');
+    messageBox.appendChild(p);
+    messageBox.scrollTop = messageBox.scrollHeight;
 
   }
 
